c7af71c5c41588c6c2f24d45cd841616880d422e
InnaHyrych <innainna3185@gmail.com>
2023-02-16 17:44:46 +0000

    Enter the name, greetings and rules of the game

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print('     Rock Paper Scissors\n')
+print('--------------------------------\n')
+print('Hi, {users_name} Welcome to our game!\n')  # add users name
+print('Make your choice - Rock, paper, or scissors.\n')
+print('     Remember:\n')
+print('* Rock wins against scissors.\n')
+print('* Scissors win against paper.\n')
+print('* Paper wins against rock.\n')
